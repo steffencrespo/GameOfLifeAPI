ebd546639adb1ea8948e1780faf7a7f406632172
leo steffen <leonardosteffen@leos-MacBook-Pro.local>
2025-05-04 20:15:44 -0300

    chore: creates docker file; changes Program.cs to display swagger in prod envs

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+﻿# Stage 1: Build
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+WORKDIR /app
+
+# Copy solution and projects
+COPY GameOfLifeSolution.sln .
+COPY src/GameOfLifeAPI/*.csproj ./src/GameOfLifeAPI/
+COPY src/GameOfLifeAPI.Tests/*.csproj ./src/GameOfLifeAPI.Tests/
+
+# Restore dependencies
+RUN dotnet restore GameOfLifeSolution.sln
+
+# Copy the rest
+COPY . .
+
+# Build and publish
+WORKDIR /app/src/GameOfLifeAPI
+RUN dotnet publish -c Release -o /app/publish
+
+# Stage 2: Runtime
+FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS runtime
+WORKDIR /app
+COPY --from=build /app/publish .
+ENTRYPOINT ["dotnet", "GameOfLifeAPI.dll"]
--- src/GameOfLifeAPI/Program.cs
+++ src/GameOfLifeAPI/Program.cs
@@ -2,23 +2,25 @@ using GameOfLifeAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton<BoardService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.WebHost.UseUrls("http://0.0.0.0:80");
+
+builder.Services.AddSingleton<IBoardService, BoardService>();
 
 var app = builder.Build();
 
-var boardService = app.Services.GetRequiredService<BoardService>();
+var boardService = app.Services.GetRequiredService<IBoardService>();
 boardService.RetrieveBoardsFromLocalStorage(); // to fetch board data from local file 
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment()) // removed so that swagger runs in the docker container prod/release 
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection(); // ignoring https redirect to be able to run in a docker container
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
